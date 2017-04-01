ccb7f2c7b5e3272afa10905e36682398d6534b44
DESKTOP-RDKHQ1D\Zaryaninov Yury <murisuu@gmail.com>
2017-04-01 13:08:09 +0900

    first commit

---
+++ testytest/1.py
@@ -0,0 +1,10 @@
+import urllib.request
+
+u = urllib.request.urlopen('http://ctabustracker.com/bustime/map/getStopPredictions.jsp?stop=14787&route=22')
+data = u.read()
+
+from xml.etree.ElementTree import XML
+doc = XML(data)
+
+for pt in doc.findall('.//pt'):
+    print(pt.text)
