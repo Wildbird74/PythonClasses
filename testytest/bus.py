4d9c9f33b5b89da75fe4b940b78eb8a18353f0b8
DESKTOP-RDKHQ1D\Zaryaninov Yury <murisuu@gmail.com>
2017-04-01 13:37:58 +0900

    Push test

--- testytest/bus.py
+++ testytest/bus.py
@@ -1,3 +1,5 @@
+# Project Catching the Bus
+# https://www.safaribooksonline.com/library/view/python-programming-language/9780134217314/PYMC_01_02.html
 import urllib.request
 
 u = urllib.request.urlopen('http://ctabustracker.com/bustime/map/getStopPredictions.jsp?stop=14787&route=22')
@@ -8,5 +10,3 @@ doc = XML(data)
 
 for pt in doc.findall('.//pt'):
     print(pt.text)
-
-#dfdfvdfvsfsfsdfsef
